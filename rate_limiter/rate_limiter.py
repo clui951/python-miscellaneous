85e5f46f53235361412d82e9e8f424dd584da056
clui951 <clui951@gmail.com>
2018-10-09 16:29:35 -0700

    rate limiter more comments

--- rate_limiter/rate_limiter.py
+++ rate_limiter/rate_limiter.py
@@ -5,26 +5,26 @@
 # assert rateLimiter.canSend(100, 3) == True #true
 # assert rateLimiter.getRate(100) == 3
 # assert rateLimiter.canSend(200, 4) == True # true
-# assert rateLimiter.getRate(100) == 7
+# assert rateLimiter.getRate(200) == 7
 # assert rateLimiter.canSend(300, 9) == False # false
-# assert rateLimiter.getRate(100) == 7
+# assert rateLimiter.getRate(300) == 7
 # assert rateLimiter.canSend(400, 1) == True # true
-# assert rateLimiter.getRate(100) == 8
+# assert rateLimiter.getRate(400) == 8
 # assert rateLimiter.canSend(2000, 10) == True# true
-# assert rateLimiter.getRate(100) == 10
+# assert rateLimiter.getRate(2000) == 10
 
-# // /*class RateLimiter {
-# //   // Set maxRate in Bytes per second
-# //     RateLimiter(int maxBytes){   
-# //     }
-# //     // Returns true if sending bytes at time = timestamp (milliseconds) will not exceed the maxRate in last second
-# //     boolean canSend(long timestamp, int bytes) {            
-# //      }
+# /*class RateLimiter {
+#    // Set maxRate in Bytes per second
+#      RateLimiter(int maxBytes){   
+#      }
+#      // Returns true if sending bytes at time = timestamp (milliseconds) will not exceed the maxRate in last second
+#      boolean canSend(long timestamp, int bytes) {            
+#      }
 
-# //     // Returns the total Bytes sent out in the last second at time = timestamp (milliseconds)
-# //     long getRate(long timestamp) {
-# //     }
-# // }*/
+#      // Returns the total Bytes sent out in the last second at time = timestamp (milliseconds)
+#      long getRate(long timestamp) {
+#      }
+# }*/
 
 class RateLimiter:
     def __init__(self, maxBytes):
@@ -82,6 +82,9 @@ assert rateLimiter.getRate(100) == 10
 
 # downsides of design
 #   centralized bottleneck across threads/machines
+#   parallel locking issues
+#   memory of computer to hold queue
+#   maybe can loosen requirements, bucket the requests, fuzzy borders, (circular array of length bytesMax)
 
 # O(# requests / second) ; O(n)
 
