134f21641ca7b2359ae8266edd2b6ca4f4ad9d17
clui951 <clui951@gmail.com>
2018-10-11 10:11:27 -0700

    17_letter_combinations_of_phone_number comments

--- 17_letter_combinations_of_phone_number/17_letter_combinations_of_phone_number.py
+++ 17_letter_combinations_of_phone_number/17_letter_combinations_of_phone_number.py
@@ -9,8 +9,8 @@ class Solution:
                         "9" : ["w", "x", "y", "z"]}
     
     # calls recursive helper that yields (generators) combinations of letters starting from specified index to the end
-    # space is O(N) where N is the length of digits, because we just need to track N generators
-    # time is O(N) also, because for every index, we only call the helper once, and yield O(1), 3 or 4, elements per call
+    # space is O(3^N) where N is the length of digits, because we build up a list of the combinations; we need to track N generators
+    # time is O(#^N) also, because we generate each combination; every index, we only call the helper once, and yield O(1), 3 or 4, elements per call
     def letterCombinations(self, digits):
         """
         :type digits: str
